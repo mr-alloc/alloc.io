56744f1a0778a8f6ceec87385de335316d3fa10e
mr-alloc <mr.allocater@gmail.com>
2023-03-23 23:10:46 +0900

    generate 옵션 삭제

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-    ssr: false,
-    generate: {
-        dir: 'dist'
-    },
     target: 'static',
     router: {
         base: '/special-posted-in/'
