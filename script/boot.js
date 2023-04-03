f09806a2c12c3bd51e93370cb1e63c52fdd44462
mr-alloc <mr.allocater@gmail.com>
2023-04-03 23:52:43 +0900

    register dynamic route paths

--- script/boot.js
+++ script/boot.js
@@ -5,22 +5,30 @@ const refresh = require('./refresh')
 const __ROOT__ = process.env.PWD
 const __DOCS__ = '/docs'
 const __FILE_NODE__ = `${__ROOT__}/static/file-node.json`
-const __POST_MAP__ = `${__ROOT__}/static/posts.json`
+const __POSTS__ = `${__ROOT__}/static/posts.json`
+const __KEYS__ = `${__ROOT__}/static/keys.json`
 
 /* explore with recursive */
 const fileNode = explore(__DOCS__)
-
+const routePaths = new Array()
 /* Sort and indexing */
 const posts = postStore.sort((a, b) => b.header.date - a.header.date)
     .map((post) => {
+        routePaths.push(post._path)
         return post
     });
-
 /* explored data for navigate */
 refresh(__FILE_NODE__, fileNode)
 
 /* posting list data */
-refresh(__POST_MAP__, posts)
+refresh(__POSTS__, posts)
+
+/* routing paths for routing */
+refresh(__KEYS__, routePaths)
+
+
+
+
 
 
 
