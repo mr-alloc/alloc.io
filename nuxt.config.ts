4dec4d9d10b85cf3c84a5dee0a490bf4614ed233
mr-alloc <mr.allocater@gmail.com>
2023-04-04 23:56:02 +0900

    add keys

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,7 @@ export default defineNuxtConfig({
     ssr: false,
     generate: {
         dir: 'dist',
+        routes: Array.from(keys)
     },
     router: {
         base: '/special-posted-in/',
