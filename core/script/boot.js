c8ea964c0241c2d7627127664cdb4ec3f48eb7f5
mr-alloc <mr.allocater@gmail.com>
2023-04-09 23:51:00 +0900

    add sitemap creator

--- core/script/boot.js
+++ core/script/boot.js
@@ -1,12 +1,14 @@
 const explore = require('./explore')
 const postStore = require('./postStore')
 const refresh = require('./refresh')
+const createSitemap = require('./createSitemap')
 
 const __ROOT__ = process.env.PWD
 const __DOCS__ = '/docs'
 const __FILE_NODE__ = `${__ROOT__}/static/file-node.json`
 const __POSTS__ = `${__ROOT__}/static/posts.json`
 const __KEYS__ = `${__ROOT__}/static/keys.json`
+const __SITEMAP__ = './public/sitemap.xml'
 
 /* explore with recursive */
 const fileNode = explore(__DOCS__)
@@ -17,6 +19,13 @@ const posts = postStore.sort((a, b) => b.header.date - a.header.date)
         routePaths.push(post._path)
         return post
     });
+
+createSitemap().then((data) => {
+    const file = data.toString().replace(/\\"/g, '"')
+    console.log(file)
+    refresh(__SITEMAP__, file)
+})
+
 /* explored data for navigate */
 refresh(__FILE_NODE__, fileNode)
 
