fde76d395c5f7d2c4524eec1ae3eace3c32a836e
mr-alloc <mr.allocater@gmail.com>
2023-06-24 19:09:18 +0900

    chage domain

--- core/script/createSitemap.js
+++ core/script/createSitemap.js
@@ -11,7 +11,7 @@ module.exports = () => {
         return link
     })
     const stream = new SitemapStream({
-        hostname: 'https://taech.io'
+        hostname: 'https://tech.salar.kr'
     })
     return streamToPromise(Readable.from(links).pipe(stream))
 }
