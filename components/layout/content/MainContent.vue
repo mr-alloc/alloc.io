0001e015e8cd66a670d21383ccce0b34255b3dcf
mr-alloc <mr.allocater@gmail.com>
2023-07-11 21:48:40 +0900

    검색창 디자인 추가

--- components/layout/content/MainContent.vue
+++ components/layout/content/MainContent.vue
@@ -2,7 +2,30 @@
   <div class="main-content-view" id="main-content-wrapper">
     <nuxt-page class="current-content" :page-key="route.fullPath" />
     <MainFooter />
-    <div class="background" :class="{ active : data.mobileNaviStore.isActive}" v-on:click="data.mobileNaviStore.isActive = false">
+    <div class="background" :class="{ active : data.mobileNaviStore.isActive || !searchStatus.isSearchMode }" v-on:click="data.mobileNaviStore.isActive = false">
+      <div v-if="!searchStatus.isSearchMode" class="search-result-area">
+        <div class="search-result-panel">
+          <div class="search-result-row">
+            <div class="category-icon-area">
+              <span class="category-icon">
+                <img src="@/assets/icon/algorithm.png"/>
+              </span>
+            </div>
+            <div class="detected-content-area">
+              <div class="result-string">
+                <span>검색을 좋아하는 나는 <strong>육포</strong>를 좋아한다.</span>
+              </div>
+              <div class="result-breadcrumb">
+                <span>
+                  검색
+                <font-awesome-icon :icon="['fas', 'chevron-right']"/>
+                  2022년 07월 21일 일기
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -11,8 +34,9 @@
 import MainFooter from "@/components/layout/content/MainFooter.vue";
 import { mobileNaviStore } from "@/store";
 import { useRoute } from "#app";
+import {useSearchStatusStore} from "~/store/SearchStatusStore";
 const route = useRoute()
-
+const searchStatus = useSearchStatusStore()
 const components = {
   MainFooter
 }
@@ -49,6 +73,41 @@ const data = {
       background: rgb(0 0 0 / 40%);
       height: 100%;
 
+    }
+
+    .search-result-area {
+      position: relative;
+      top: 100px;
+      width: 100%;
+      height: 70%;
+
+      .search-result-panel {
+        background-color: white;
+        max-width: 768px;
+        height: 100%;
+        margin: 0 auto;
+
+        .search-result-row {
+          display: flex;
+          flex-direction: row;
+
+          .category-icon-area {
+            flex-shrink: 0;
+            width: 40px;
+            justify-content: center;
+            align-items: center;
+
+            .category-icon {
+              display: inline-block;
+
+              img {
+                width: $small-icon-size;
+              }
+            }
+          }
+        }
+
+      }
     }
   }
 
---
+++ store/SearchStatusStore.ts
@@ -0,0 +1,17 @@
+import {defineStore} from "pinia";
+
+export const useSearchStatusStore = defineStore('searchStatus', () => {
+    const isSearchMode = ref(false)
+
+    function searching() {
+        isSearchMode.value = true;
+    }
+
+    function cancelSearch() {
+        isSearchMode.value = false
+    }
+
+    return {
+        isSearchMode, searching, cancelSearch
+    }
+})
