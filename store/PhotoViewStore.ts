c30f138c219c38f6c01ddf1f7afd017f6bd43437
mr-alloc <mr.allocater@gmail.com>
2023-10-26 21:20:49 +0900

    add photoview feature

--- store/PhotoViewStore.ts
+++ store/PhotoViewStore.ts
@@ -1,36 +1,103 @@
 import {defineStore} from "pinia";
-import {toKeyMap} from 'utils/settingUtils'
+import {toKeyMap} from '~/utils/settingUtils'
 import {Image} from "~/class/implement/Image";
+import {initialize} from "esbuild";
 
-export const usePhotoViewStore = defineStore('photoView', () => {
+export const usePhotoViewStatusStore = defineStore('photoViewStatus', () => {
     const isPhotoView = ref<boolean>(false)
+    const isFullScreen = ref<boolean>(false)
     const images = ref<Map<number, Image>>(new Map())
-    const current = ref<Image>(new Image('', '이미지 불러오는 중'))
+    const zoom = ref(1)
+    const currentIndex = ref(0)
+
+    const maxZoom = 5
+    const minZoom = 1
 
     function load(toBeLoaded: Array<Image>) {
-        images.value = toKeyMap<number, Image>(toBeLoaded, (e, i) => i)
+        clear()
+        images.value = toKeyMap<number, Image>(toBeLoaded, (e, i) => i + 1)
+        images.value.size > 0 && (currentIndex.value = 1)
+    }
+
+    function clear() {
+        zoom.value = 1
+        isFullScreen.value = false
     }
 
     function open(index: number) {
         if (images.value.size == 0) return
-        current.value = images.value.get(index)!
+        currentIndex.value = index
+        if (isPhotoView.value) {
+            return
+        }
         isPhotoView.value = true
+        const html = document.querySelector('html')!
+        html.style.overflow = 'hidden'
+        document.body.style.overflow = 'hidden'
     }
 
     function close() {
         isPhotoView.value = false
+        const html = document.querySelector('html')!
+        html.style.overflow = 'unset'
+        document.body.style.overflow = 'unset'
     }
 
-    function hasCurrent(): boolean {
-        return current.value !== null
+    function zoomIn() {
+        maxZoom > zoom.value && zoom.value++
     }
 
-    return {
-        isPhotoView,
-        current,
-        load,
-        open,
+    function zoomOut() {
+        minZoom < zoom.value && zoom.value--
+    }
+
+    function nextImage() {
+        hasNext() && currentIndex.value++
+    }
+
+    function hasNext() {
+        return images.value.size !== 0 && currentIndex.value < images.value.size
+    }
+
+    function beforeImage() {
+        hasBefore() && currentIndex.value--
+    }
+
+    function hasBefore() {
+        return images.value.size !== 0 && currentIndex.value > 1
+    }
+
+    function current() {
+        return images.value.has(currentIndex.value)
+            ? images.value.get(currentIndex.value)
+            : new Image('https://placehold.co/200x600', '이미지 불러오는 중')
+    }
+
+    function imageList() {
+        return [...images.value.entries()]
+            .sort((a, b) => a[0] - b[0])
+            .map(([k, v]) => v)
+    }
+
+    function activateFullScreen() {
+        isFullScreen.value = true
+    }
+
+    function inactivateFullScreen() {
+        isFullScreen.value = false
+    }
+
+
+    return { isPhotoView, isFullScreen, currentIndex, zoom, imageList, load, open,
         close,
-        hasCurrent
+        zoomIn,
+        zoomOut,
+        beforeImage,
+        nextImage,
+        current,
+        hasNext,
+        hasBefore,
+        activateFullScreen,
+        inactivateFullScreen
     }
 })
