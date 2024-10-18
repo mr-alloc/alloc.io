2f2c99878bda1291e7c21fe70e37cdc1e9e3b918
mr-alloc <mr.allocater@gmail.com>
2024-10-18 17:00:40 +0900

    임시배포

--- src/plugins/MarkdownIt.ts
+++ src/plugins/MarkdownIt.ts
@@ -2,6 +2,7 @@ import MarkdownIt from "markdown-it";
 import {DEFAULT_MARKDOWN_IT_OPTIONS} from "@/utils/MarkdownUtils";
 import DecoratorProvider from "@/markup/decorator/DecoratorProvider";
 import RuleType from "@/markup/constant/RuleType";
+import sh from "shiki";
 import shiki from "@shikijs/markdown-it";
 
 export default defineNuxtPlugin(async (nuxtApp) => {
@@ -11,12 +12,24 @@ export default defineNuxtPlugin(async (nuxtApp) => {
     DecoratorProvider.provide(RuleType.BLOCK_QUOTE).decorate(markdownIt);
     DecoratorProvider.provide(RuleType.HEADLINE).decorate(markdownIt);
     DecoratorProvider.provide(RuleType.CODE_BLOCK).decorate(markdownIt);
+    const highlighter = await sh.getSingletonHighlighter();
 
     markdownIt.use(await shiki({
+        transformers: [
+            {
+                code(hast) {
+                    hast.properties.className = [
+                        ...(hast.properties.className as string [] || []),
+                        'text-nowrap'
+                    ];
+                    return hast;
+                },
+            }
+        ],
         themes: {
             light: 'catppuccin-frappe',
             dark: 'catppuccin-mocha'
-        }
+        },
     }));
 
     nuxtApp.provide('md', markdownIt);
