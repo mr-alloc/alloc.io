4b51d1353d0190c5c3854e34c4b7545aa25bec42
mr-alloc <mr.allocater@gmail.com>
2024-12-16 18:45:43 +0900

    포스트목록 fallback 수정

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="relative py-0 sm:py-16 max-w-md z-30" id="main-content-body">
       <div class="grid grid-cols-1 sm:grid-cols-1 xl:grid-cols-1 gap-8">
-        <ClientOnly fallback-tag="span" fallback="Loading Posts...">
+        <ClientOnly :fallback="PostContentLoader">
           <PostCard :feed="feed" v-for="(feed, index) in appCache.feeds" v-bind:key="index" />
         </ClientOnly>
       </div>
@@ -13,6 +13,7 @@ import {callPostFeed} from "@/utils/PostUtil";
 import appCache from '@/store/appCache'
 import PostCard from '@/components/layout/content/post-card/PostCard.vue'
 import {onMounted} from "vue";
+import PostContentLoader from "@/components/layout/content/post-card/PostContentLoader.vue";
 
 useHead({
   titleTemplate: (titleChunk) => {
