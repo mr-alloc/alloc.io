344c0e4feb6f92543de86ebe7c39723f4639dfd8
mr-alloc <mr.allocater@gmail.com>
2024-12-21 17:52:01 +0900

    chore: change domain for pages.

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
     fullName: "devisitem",
     username: "devis",
-    domain: "https://devis.kr",
+    domain: "https://alloc.io",
     title: "DEVIS의 기술블로그",
     description: "불편해야 성장할 수 있어.",
     defaultProfile: "https://avatars.githubusercontent.com/u/65699391?v=4",
