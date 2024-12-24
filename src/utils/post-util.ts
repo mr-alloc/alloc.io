dc7b6035ced25ac65c337d0dd1cb2a7b44309724
mr-alloc <mr.allocater@gmail.com>
2024-12-24 20:55:20 +0900

    chore: add thumbnail image to os post.

--- src/utils/post-util.ts
+++ src/utils/post-util.ts
@@ -6,8 +6,8 @@ import {usePostCallStore} from "@/store/post-call-store";
 const DEFAULT_FEED_SIZE = 4
 export const callPostFeed = (): void => {
     /* 최대 feed 사이즈 만큼 호출*/
-    for(let i = 0; (i < DEFAULT_FEED_SIZE && appCache.postContents.length != 0); i++){
-        const post = appCache.postContents.shift()
+    for(let i = 0; (i < DEFAULT_FEED_SIZE && appCache.postContents.length != 0); i++) {
+        const post = appCache.postContents.shift();
         if (post?.header.layout === 'post') {
             appCache.feeds.push(post)
         }
