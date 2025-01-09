e283262ecb0edc7022ac3ec2d4e84a5308458163
mr-alloc <mr.allocater@gmail.com>
2025-01-10 08:38:54 +0900

    fix: remove conflict string from merged

--- src/plugins/markdown-it.ts
+++ src/plugins/markdown-it.ts
@@ -5,17 +5,6 @@ import RuleType from "@/markup/constant/rule-type";
 import shiki from "@shikijs/markdown-it";
 
 export default defineNuxtPlugin(async (nuxtApp) => {
-<<<<<<< HEAD
-<<<<<<< HEAD:src/plugins/markdown-it.ts
-=======
-    console.log('md: ', nuxtApp.$md);
-=======
-
-    //다른곳에서 nuxtApp.$md 호출시 나오지 않는경우, 플러그인이 로드되지 않아서일 수 있다.
-    //이경우 plugins 디렉터리 하위에 바로 위치 하는지 확인이 필요하다.
->>>>>>> 6f46ebc (debug: change markdown-it plugin position. (unloaded))
-
->>>>>>> ca726a4 (debug: markdown-it module undefined):src/plugins/markdown-it/markdown-it.ts
     if (nuxtApp.$md) return;
     const markdownIt = new MarkdownIt(DEFAULT_MARKDOWN_IT_OPTIONS);
 
